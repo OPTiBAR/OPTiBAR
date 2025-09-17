4ad64a93675af1ece130b240e9f94370d490d2f1
Sina Hajikazemi <sina.hkazemi@gmail.com>
2025-09-18 00:19:19 +0200

    url deleted

--- src/gui/control/body/calculation.py
+++ src/gui/control/body/calculation.py
@@ -1,20 +1,19 @@
 import tkinter as tk
 from typing import Dict
-import requests
 from src.control.utils.urls import connection_error, get_header, get_url
-from src.view.utils.router import Router
-from src.control.utils.threading import tr_request, Monitor
+from src.view.utils.registry import Registry
 from tkinter.messagebox import showinfo, showerror
 from .special_lengths import Rebar
 
 class Calc():
-    def __init__(self, root: Router, token_var: tk.StringVar, project_data: Dict, result_data: Dict) -> None:
-        self.root = root
-        calc_note = root.get_child('notebook').get_child('calc')
-        self.calc_type = calc_note.get_child('type')
-        self.setting = calc_note.get_child('setting')
-        self.request_frame = calc_note.get_child('request')
-        self.special_lengths = self.setting.get_child('advanced').get_child('special-lengths')
+    def __init__(self, registry: Registry, project_data: Dict, result_data: Dict) -> None:
+        self.registry = registry
+        calc_note = registry.get('calc')
+        self.calc_type = registry.get('type')
+        self.setting = registry.get('setting')
+        self.request_frame = registry.get('request')
+        # self.special_lengths = self.setting.get_child('advanced').get_child('special-lengths')
+        self.special_lengths = registry.get('special-lengths')
 
 
         # bind to events
@@ -25,7 +24,6 @@ class Calc():
 
         # variables
         self.result_data = result_data
-        self.token_var = token_var
         self.project_data = project_data
         project_data['is_available'].trace_add('write', self.project_data_changed)
 
@@ -71,152 +69,20 @@ class Calc():
             self.reset_pressed()
     
     def calc_type_changed(self, *args):
-        calc_widget = self.root.get_child('notebook').get_child('calc')
-        calc_type_value = calc_widget.get_child('type').get_value()
+        # calc_widget = self.root.get_child('notebook').get_child('calc')
+        # calc_type_value = calc_widget.get_child('type').get_value()
+        calc_widget = self.registry.get('calc')
+        calc_type_value = self.registry.get('type').get_value()
         if calc_type_value == 'analysis':
             calc_widget.get_child('setting').get_child('basic').set_analysis_layout()
         elif calc_type_value == 'calculation':
             calc_widget.get_child('setting').get_child('basic').set_calc_layout()
 
-    def _stop_monitor(self):
-        if hasattr(self, 'monitor'):
-            self.monitor.stop()
-        self.monitor = Monitor(self.root)
-        if hasattr(self, 'stop_flag'):
-            self.stop_flag.set(True)
-        self.stop_flag = tk.BooleanVar()
-        self.stop_flag.set(False)
-
 
     def calc_pressed(self, *args):
         calc_type = self.calc_type.get_value() 
         stop_flag = self.stop_flag # a local copy of
-        iter_counter = 0 
-
-        def create_order_request():
-            setting_values = {'config_data': self.setting.get_values()}
-            calc_type = self.calc_type.get_value()
-            if calc_type == 'calculation':
-                url = get_url('calculation_orders', project_id = self.project_data['data']['project_id'])
-            elif calc_type == 'analysis':
-                url = get_url('analysis_orders', project_id = self.project_data['data']['project_id'])
-            else:
-                raise ValueError('calc type is not valid')
-            
-            thread = tr_request(method='post', url=url, json=setting_values, headers=get_header(self.token_var))
-            self.monitor.run(thread, handle_create_order)
-        
-        def handle_create_order(response: requests.Response):
-            if response is not None:
-                if  response.status_code in (200,201):
-                    self.order_id = response.json()['id']
-                    self.request_frame.set_status('Waiting in the queue')
-                    get_order()
-                else:
-                    stop_calc()
-                    if response.status_code == 403:
-                        showerror('Service Level', response.json()['detail'])
-                    elif response.status_code == 404:
-                        pass
-                    else:
-                        raise ValueError(f'unhandled status code {response.status_code}')
-            elif not stop_flag.get():
-                connection_error()
-                        
-
-            
-        def retrieve_order():
-            print('retrieve called')
-            if calc_type == 'calculation':
-                url = get_url('calculation_orders_id', project_id = self.project_data['data']['project_id'], order_id=self.order_id)
-            elif calc_type == 'analysis':
-                url = get_url('analysis_orders_id', project_id = self.project_data['data']['project_id'], order_id=self.order_id)
-            else:
-                raise ValueError('calc type is not valid')
-            thread = tr_request(method='get', url=url, headers=get_header(self.token_var))
-            self.monitor.run(thread, handle_retrieve_order)
-        
-        def handle_retrieve_order(response: requests.Response):
-            if response is not None:
-                if  response.status_code == 200:
-                    if response.json()['status'] == 'WA':
-                        self.request_frame.set_status('Waiting in the queue')
-                    elif response.json()['status'] == 'ST':
-                        if calc_type == 'analysis':
-                            self.request_frame.set_status(f'Started, Progress: {response.json()["progress"]}')
-                        else:
-                            self.request_frame.set_status('Started')
-                    elif response.json()['status'] == 'FN':
-                        self.result_data['data'] = {
-                            'order_id' : self.order_id,
-                            'calc_type': calc_type,
-                            'result_data' : response.json()['result_data'],
-                        }
-                        self.request_frame.set_status('Finished')
-                        stop_calc()
-                        showinfo('Calculation', 'Calculated successfuly. go to the 3. Results tab to observe the results.')
-                        # comes last to show mass limit info after the successful calculation message.
-                        self.result_data['is_available'].set(True)
-                    else:
-                        raise ValueError(f'unhandled status value {response.json()["status"]}')
-                else:
-                    raise ValueError(f'unhandled status code {response.status_code}')
-            elif not stop_flag.get():
-                self.request_frame.set_status('Connection Problem ...')
-
-            
-        
-        def get_order():
-            if not stop_flag.get():
-                nonlocal iter_counter
-                print(iter_counter)
-                retrieve_order()
-                if iter_counter < 5:
-                    interval = 5000
-                elif iter_counter < 10:
-                    interval = 10000
-                else:
-                    interval = 20000
-                self.root.after(interval, get_order)
-                iter_counter += 1
-
-
-        def delete_order():
-            print('delete order called')
-            if calc_type == 'calculation':
-                url = get_url('calculation_orders_id', project_id = self.project_data['data']['project_id'], order_id=self.order_id)
-            elif calc_type == 'analysis':
-                url = get_url('analysis_orders_id', project_id = self.project_data['data']['project_id'], order_id=self.order_id)
-            else:
-                raise ValueError('calc type is not valid')
-            thread = tr_request(method='delete', url=url, headers=get_header(self.token_var))
-            self.monitor.run(thread, handle_delete_order)
-        
-        def handle_delete_order(response: requests.Response):
-            print('handle delete order called')
-            if response is not None:
-                if  response.status_code == 204:
-                    print('order deleted successfuly')    
-                else:
-                    raise ValueError(f'unhandled status code {response.status_code}')
-
-        def stop_calc():
-            self._stop_monitor()
-            self.request_frame.set_button_label('Calculate')
-
-        if self.request_frame.get_button_label() == 'Calculate':
-            self.order_id = None
-            self.result_data['is_available'].set(False)
-            self.result_data['data'] = None
-
-            self.request_frame.set_button_label('Stop')
-            create_order_request()
-        else:
-            self.request_frame.set_status("Ready")
-            stop_calc()
-            if self.order_id is not None:
-                delete_order()
-
+        iter_counter = 0
 
 
 

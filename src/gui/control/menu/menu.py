ae6a6091ca3de129da18f246aa66412a4b601881
Sina Hajikazemi <sina.hkazemi@gmail.com>
2025-09-18 00:05:34 +0200

    user deleted

--- src/gui/control/menu/menu.py
+++ src/gui/control/menu/menu.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter.constants import SE
 
 from .help import HelpMenu
-from .user import UserMenu
 from .setting import SettingMenu
 
 
@@ -10,7 +9,6 @@ class MenuBar():
     def __init__(self, root, token_var: tk.StringVar, path_var: tk.StringVar) -> None:
         self.root = root
         HelpMenu(root)
-        UserMenu(root, token_var)
         SettingMenu(root, path_var)
         
 
